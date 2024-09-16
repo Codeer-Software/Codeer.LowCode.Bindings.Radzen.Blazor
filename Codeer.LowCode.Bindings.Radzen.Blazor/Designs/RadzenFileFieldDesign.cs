a30fadaf0b65606f7655fa93825eb7c42f0a59d7
yutaka <tsumori-yutaka@codeer.co.jp>
2024-09-17 01:24:50 +0900

    FileField component

---
+++ Codeer.LowCode.Bindings.Radzen.Blazor/Designs/RadzenFileFieldDesign.cs
@@ -0,0 +1,12 @@
+﻿using Codeer.LowCode.Bindings.Radzen.Blazor.Components;
+using Codeer.LowCode.Blazor.Repository.Design;
+
+namespace Codeer.LowCode.Bindings.Radzen.Blazor.Designs
+{
+    [DisableBulkDataUpdate]
+    public class RadzenFileFieldDesign : FileFieldDesign
+    {
+        public RadzenFileFieldDesign() => TypeFullName = typeof(RadzenFileFieldDesign).FullName!;
+        public override string GetWebComponentTypeFullName() => typeof(RadzenFileFieldComponent).FullName!;
+    }
+}
