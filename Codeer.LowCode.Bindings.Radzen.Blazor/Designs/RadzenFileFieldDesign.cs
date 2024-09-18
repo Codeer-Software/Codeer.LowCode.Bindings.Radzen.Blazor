5ca4eb9b2dbe8494792b37254fbc833793835e12
yutaka <tsumori-yutaka@codeer.co.jp>
2024-09-18 17:01:56 +0900

    FileFieldのサンプルを修正、スタイリング機能を追加

--- Codeer.LowCode.Bindings.Radzen.Blazor/Designs/RadzenFileFieldDesign.cs
+++ Codeer.LowCode.Bindings.Radzen.Blazor/Designs/RadzenFileFieldDesign.cs
@@ -1,4 +1,5 @@
 ﻿using Codeer.LowCode.Bindings.Radzen.Blazor.Components;
+using Codeer.LowCode.Bindings.Radzen.Blazor.Enums;
 using Codeer.LowCode.Blazor.Repository.Design;
 
 namespace Codeer.LowCode.Bindings.Radzen.Blazor.Designs
@@ -6,6 +7,12 @@ namespace Codeer.LowCode.Bindings.Radzen.Blazor.Designs
     [DisableBulkDataUpdate]
     public class RadzenFileFieldDesign : FileFieldDesign
     {
+        [Designer]
+        public RadzenVariant Variant { get; set; }
+
+        [Designer]
+        public RadzenColor Color { get; set; }
+
         public RadzenFileFieldDesign() => TypeFullName = typeof(RadzenFileFieldDesign).FullName!;
         public override string GetWebComponentTypeFullName() => typeof(RadzenFileFieldComponent).FullName!;
     }
