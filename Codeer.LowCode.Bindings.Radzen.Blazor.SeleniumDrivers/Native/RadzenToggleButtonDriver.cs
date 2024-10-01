a2787fc5fad6a91ae44c5e500aff5d5082254c19
yutaka <tsumori-yutaka@codeer.co.jp>
2024-10-01 11:04:49 +0900

    Fix toggle button driver

--- Codeer.LowCode.Bindings.Radzen.Blazor.SeleniumDrivers/Native/RadzenToggleButtonDriver.cs
+++ Codeer.LowCode.Bindings.Radzen.Blazor.SeleniumDrivers/Native/RadzenToggleButtonDriver.cs
@@ -5,7 +5,7 @@ namespace Codeer.LowCode.Bindings.Radzen.Blazor.SeleniumDrivers.Native
 {
     public class RadzenToggleButtonDriver(IWebElement element) : ControlDriverBase(element)
     {
-        public bool Checked => Element.FindElement(By.TagName("input")).GetAttribute("checked") == "checked";
+        public bool Checked => Element.FindElement(By.TagName("input")).GetAttribute("aria-checked") == "true";
 
         public void Edit(bool b)
         {
